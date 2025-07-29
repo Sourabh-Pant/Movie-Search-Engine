04a53de939581695f94a4273e6fdc24fef73bcb3
Sourabh-Pant <pantsourabh07@gmail.com>
2025-07-29 17:15:26 +0530

    Update verifyToken.js

--- backend/middlewares/verifyToken.js
+++ backend/middlewares/verifyToken.js
@@ -2,11 +2,14 @@ import jwt from 'jsonwebtoken';
 
 const verifyToken = (req, res, next) => {
   const token = req.cookies.token;
-  if (!token) return res.status(401).json({ message: "Not logged in" });
+
+  if (!token) {
+    return res.status(401).json({ message: "Not logged in" });
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Contains { id, iat, exp }
+    req.user = decoded; // contains { id, iat, exp }
     next();
   } catch (err) {
     return res.status(403).json({ message: "Invalid or expired token" });
