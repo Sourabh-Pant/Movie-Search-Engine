7dc417d92ec87f35a01f4811332c031cc8498c1a
Sourabh-Pant <pantsourabh07@gmail.com>
2025-07-29 16:49:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const port=process.env.PORT || 4000; 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://movie-search-engine-1.onrender.com',
   credentials: true
 }));
 app.use(express.json());
